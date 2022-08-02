000fcedb0ad3f9fe4733550f5b2df64e278a3ad2
Srishti Parti <srishtiparti97@gmail.com>
2022-08-02 11:38:29 -0400

    commit on main

--- 1-intro.js
+++ 1-intro.js
@@ -4,4 +4,7 @@ console.log("first file for node")
 // npm init 
 // npm init -y for default
 // npm i lodash (or any other dependecies)
+// remove package
+// npm uninstall bootstrap
+// remove the dependency from package.json and run npm install
 // to run this just type node 1.intro.js on terminal and to stop the terminal its - ctrl + c
---
+++ 8-nodemon.js
@@ -0,0 +1,11 @@
+// install nodemon - npm i nodemon -D so that we don't have to run our code everytime we make the changes
+// We use D because we just want it in dev environment and not in prod environment
+// Change the script... in debug
+
+// "scripts": {
+//     "start": "node app.js"
+//      "dev": "nodemon app.js"
+// },
+
+// instead of typing node app.js just type npm start
+//  to start nodemon - npm run dev to restart your app
